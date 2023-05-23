08d60717a64a98862514dce0f59342978a3dc1ba
Manoj Jadhav <manoj.jadhav@neosoftmail.com>
2023-05-23 13:10:17 +0530

    basic setup for nextjs_app

---
+++ pages/events/[...slug].js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function FiltterdEventsPage() {
+  return (
+    <h1>Filtterd page Filtterd events</h1>
+  )
+}
---
+++ pages/events/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AllEventsPage() {
+  return (
+    <h1>AllEventsPage</h1>
+  )
+}
